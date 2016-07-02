881b8fa3a3c870ada1e7557cb218d53f303c46c5
magxax <magxax@gmail.com>
2016-07-02 16:56:16 +0200

    changements dans Unit.hpp

--- src/Unit.hpp
+++ src/Unit.hpp
@@ -5,21 +5,38 @@
 
 using sf_Vector2sz = sf::Vector2<size_t>;
 
-struct Unit
+class Unit
 {
-    sf_Vector2sz     pos;
+	public:
+		sf_Vector2sz get_pos();
+		int get_team();
+		int get_move ();
+		int get_life ();
+		int get_range();
+		int get_firepower ();
+		int get_max_life();
+		int get_max_move();
+		int get_animLength();
+		bool isDead();
+		bool isInTransition();
+		void move();
+		void attack();
+		void takeDamage(int);
+		void update();
+	private:
+		sf_Vector2sz     pos;
 
-    int     team;
-    int     speed;
-    int     move;
-    int     max_life;
-    int     life;
-    int     range;
-    int     firepower;
+		int     team;
+		int     speed;
+		int     move;
+		int     max_life;
+		int     life;
+		int     range;
+		int     firepower;
 
-    using UPtr = std::unique_ptr<Unit>;
+		using UPtr = std::unique_ptr<Unit>;
 
-    bool isInTransition() { return false; }
+		bool isInTransition() { return false; }
 };
 
 #endif // !UNIT_HPP_
